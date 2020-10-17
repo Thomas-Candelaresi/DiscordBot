d5a769f1b0f65d55d31e9e7a1893310a8fd13be6
Thomas-Candelaresi <thomas.c@moneyboxapp.com>
2020-10-17 16:07:13 +0100

    add real initial bot

--- bot.js
+++ bot.js
@@ -1,12 +1,23 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
+var logger = require('winston');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug'
 
 const prefix = "!";
-
+logger.info('before client');
 const bot = new Discord.Client();
+logger.info('after client');
+
 
 bot.on('ready', () => {
     console.log("Salut c'est Bob. Prêt à casser des culs!");
+    logger.info("Salut c'est Bob. Prêt à casser des culs!");
     var test = bot.channels;
 });
 
@@ -18,11 +29,17 @@ bot.on("message", function (message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
-    if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    switch(command) {
+        case "ping":
+            const timeTaken = Date.now() - message.createdTimestamp;
+            message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+            break;
     }
+
+    // if (command === "ping") {
+    //     const timeTaken = Date.now() - message.createdTimestamp;
+    //     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    // }
 });
 
-//bot.login(config.BOT_TOKEN);
-bot.login(process.env.BOT_TOKEN);
+bot.login(config.BOT_TOKEN);

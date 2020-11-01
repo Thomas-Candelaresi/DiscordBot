17d930557d6a4fa1d09a687ae0a5cf248f169334
Thomas-Candelaresi <thomas.c@moneyboxapp.com>
2020-11-01 11:40:22 +0000

    i18n

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 const _discord = require("discord.js");
+const i18n = require('i18n');
+const path = require('path')
 var _logger = require('winston');
 var config = require("./config.json");
 var weapons = require("./weapons.json");
+const { loggers } = require("winston");
 var _token = null;
 try {
     _token = require("./token.json");
@@ -66,40 +69,67 @@ var addWeapon = function (message, args) {
 }
 
 var muteToggle = function (message, isMute) {
-    _logger.debug("member: " + message.member);
-    _logger.debug("voice: " + message.member.voice);
-    _logger.debug("channel: " + message.member.voice.channel);
     if (message.member.voice.channel) {
         let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
         for (const [memberID, member] of channel.members) {
             member.voice.setMute(isMute);
         }
+        return true;
     } else {
-        _logger.info('You need to join a voice channel first!');
+        return false;
+    }
+}
+
+var identifyLocale = function(message) {
+    var prefixChannel = message.channel.name?.split('-')[0];
+    switch(prefixChannel) {
+        case "en" :
+        case "moneybox" :
+            i18n.setLocale('en');
+            break;
+        case "fr":
+        default:
+            i18n.setLocale('fr');
     }
 }
 
 bot.on('ready', () => {
+    i18n.configure({
+        locales: ['en', 'fr'],
+        directory: path.join(__dirname, '/locales')
+    });
+    i18n.setLocale('fr');
     _logger.info("Salut c'est Bob. Prêt à casser des culs!");
-    var test = bot.channels;
 });
 
 bot.on("message", function (message) {
+    identifyLocale(message);
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+    const command = args.shift().toLowerCase().toString();
 
-    _logger.debug(message.content);
+    _logger.debug("command to check : " + command);
     switch (command) {
         case "ping":
             const timeTaken = Date.now() - message.createdTimestamp;
-            message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+            message.reply(i18n.__('pingMessage') + ` ${timeTaken}ms.`);
             break;
         case "help":
-            message.reply('List of available commands : \n!ping - evaluates your ping to the server. \n!help - provides the list of available commands.');
+            message.reply(i18n.__('listCommands')+ "\n" +
+            i18n.__('pingExplain') + "\n" +
+            i18n.__('muteExplain') + "\n" +
+            i18n.__('unmuteExplain'));
+            break;
+        case "mute":
+            if (!muteToggle(message, true))
+            message.reply(i18n.__('muteError'));
+            break;
+        case "unmute":
+            if (!muteToggle(message, false))
+                message.reply(i18n.__('unmuteError'));
             break;
         case "weapon":
             _logger.debug("weapon");
@@ -110,13 +140,6 @@ bot.on("message", function (message) {
             _logger.debug("addWeapon");
             _logger.debug("command: " + command + ", parameters: " + args);
             addWeapon(message, args);
-            break;
-        case "muteChannel":
-            _logger.debug("dans mute case");
-            muteToggle(message, true);
-            break;
-        case "unmuteChannel":
-            muteToggle(message, false);
             break;
     }
 });
